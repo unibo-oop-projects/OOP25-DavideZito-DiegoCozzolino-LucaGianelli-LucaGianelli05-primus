d91619f24c449cfd74388ab56430ca8a31c7387e
Davide Zito <davide.zito2@studio.unibo.it>
2026-01-22 00:37:33 +0100

    chore: Setup Git Hooks e automazione QA in Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,3 +11,21 @@ repositories {
 application {
     mainClass.set("com.primus.app.PrimusApp")
 }
+
+// Task per installare automaticamente l'hook git
+val installGitHooks by tasks.registering(Copy::class) {
+    from(File(rootProject.rootDir, "config/git/pre-commit"))
+    into(File(rootProject.rootDir, ".git/hooks"))
+    filePermissions {
+        unix("rwxrwxrwx")
+    }
+}
+
+// Fai in modo che git hooks vengano installati ogni volta che compili o fai check
+tasks.named("check") {
+    dependsOn(installGitHooks)
+}
+
+tasks.named("compileJava") {
+    dependsOn(installGitHooks)
+}
